aab84fb24c3ee98258f914da60d3f774536a0398
Kangxu Wang <kangxu@umich.edu>
2020-07-16 22:32:25 -0700

    Implement visitors count

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const requestIp = require("request-ip");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
+const routes = require('./routes')
 
 app.use(helmet());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(requestIp.mw());
 
-app.get('/', async (req, res, next) => {
-  res.json({
-    res: 'hi'
-  });
-});
+app.use('/', routes);
 
 app.use((err, req, res, next) => {
   if (err.status) {
@@ -26,8 +25,8 @@ app.use((err, req, res, next) => {
   }
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '' : err.stack,
-  })
+    stack: process.env.NODE_ENV === "production" ? "" : err.stack,
+  });
 });
 
 const port = process.env.PORT || 4000;
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const monk = require("monk");
+
+const db = monk(process.env.MONGODB_URI);
+const visitors = db.get("visitors");
+visitors.createIndex({ ip: 1 }, { unique: true });
+
+router.get("/", async (req, res, next) => {
+  res.json({
+    res: "hi",
+    ip: req.clientIp,
+  });
+});
+
+router.get("/visitors", async (req, res, next) => {
+  let visitor;
+  try {
+    visitor = await visitors.findOneAndUpdate(
+      { ip: req.clientIp },
+      { $inc: { visit: 1 } },
+      { returnNewDocument: true }
+    );
+    if (visitor === null) {
+      visitor = await visitors.insert({
+        ip: req.clientIp,
+        visit: 1,
+      });
+    }
+  } catch (err) {
+    next(err);
+    return;
+  }
+
+  res.json({
+    visitor: visitor,
+  });
+});
+
+module.exports = router
